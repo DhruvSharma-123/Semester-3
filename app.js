1f01c35456692e570c9e96e98f08af8086bb5fce
Dhruv Sharma <sdhruv.mdnr@gmail.com>
2026-07-31 10:51:47 +0530

    StartNodejs

---
+++ app.js
@@ -0,0 +1,4 @@
+module.exports   = {
+  name: "Dhruv Sharma",
+  branch: "A.I.L.M",
+};
---
+++ index.js
@@ -0,0 +1,31 @@
+// var a=20;
+// var b=20;
+// var c=20;
+// console.log(a+b+c);
+// let arr=[1,2,3,4,5];
+// console.log(arr);
+// arr.splice(2,2,1,2);
+// console.log(arr);
+
+// var x=20;
+// if(x==20){
+//     console.log("X IS 20");
+// }else{
+//     console.log("X is not 20");
+// }
+
+// console.log(`may age is ${2+16}`);
+// let a=prompt("Enetr your names");
+//console.log(a);
+
+// for(let a=0;a<5;a++){
+//     console.log(a);
+// }
+
+// let arr=[1,2,3,4,5];
+// for(let a=0;a<arr.length;a++){
+//     console.log(arr[a]);
+// }
+
+const app = require('./app.js');
+console.log(app);
