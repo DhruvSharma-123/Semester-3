b025075bfe907257e1f83c6397d93df61e6bbbb7
Dhruv Sharma <sdhruv.mdnr@gmail.com>
2026-09-25 11:31:18 +0530

    setImmediate and process.nextTick

--- oops.js
+++ oops.js
@@ -1,75 +1,126 @@
-// class BankAccount{
-//     name;
-//     accountNo;
-//     constructor(name,accountNo){
-//         this.name=name;
-//         this.accountNo=accountNo;
+// // class BankAccount{
+// //     name;
+// //     accountNo;
+// //     constructor(name,accountNo){
+// //         this.name=name;
+// //         this.accountNo=accountNo;
+// //     }
+
+// //     display(){
+// //         console.log(name);
+// //         console.log(this.accountNo);
+// //     }
+// // }
+
+
+// // Parent class
+// class BankAccount {
+//     constructor(name, accountNo, balance) {
+//         this.name = name;
+//         this.accountNo = accountNo;
+//         this.balance = balance;
 //     }
 
-//     display(){
-//         console.log(name);
-//         console.log(this.accountNo);
+//     display() {
+//         console.log("Name:", this.name);
+//         console.log("Account No:", this.accountNo);
+//         console.log("Balance:", this.balance);
+//     }
+
+//     withdraw(amount) {
+//         if (amount <= this.balance) {
+//             this.balance -= amount;
+//             console.log("Withdraw Amount:", amount);
+//         } else {
+//             console.log("Insufficient Balance");
+//         }
 //     }
 // }
 
+// // Child class
+// class SalaryAccount extends BankAccount {
+//     constructor(name, accountNo, balance, salary) {
+//         super(name, accountNo, balance);
+//         this.salary = salary;
+//     }
 
-// Parent class
-class BankAccount {
-    constructor(name, accountNo, balance) {
-        this.name = name;
-        this.accountNo = accountNo;
-        this.balance = balance;
-    }
+//     calculateSalaryAfterWithdraw(amount) {
+//         this.withdraw(amount);
+//         let remainingSalary = this.salary - amount;
 
-    display() {
-        console.log("Name:", this.name);
-        console.log("Account No:", this.accountNo);
-        console.log("Balance:", this.balance);
-    }
+//         console.log("Salary:", this.salary);
+//         console.log("Salary After Withdraw:", remainingSalary);
+//     }
 
-    withdraw(amount) {
-        if (amount <= this.balance) {
-            this.balance -= amount;
-            console.log("Withdraw Amount:", amount);
-        } else {
-            console.log("Insufficient Balance");
-        }
-    }
-}
+//     display() {
+//         super.display();
+//         console.log("Salary:", this.salary);
+//     }
+// }
 
-// Child class
-class SalaryAccount extends BankAccount {
-    constructor(name, accountNo, balance, salary) {
-        super(name, accountNo, balance);
-        this.salary = salary;
-    }
+// // Object creation
+// let account = new SalaryAccount(
+//     "Dhruv",
+//     123456,
+//     50000,
+//     40000
+// );
 
-    calculateSalaryAfterWithdraw(amount) {
-        this.withdraw(amount);
-        let remainingSalary = this.salary - amount;
+// account.display();
 
-        console.log("Salary:", this.salary);
-        console.log("Salary After Withdraw:", remainingSalary);
-    }
+// account.calculateSalaryAfterWithdraw(10000);
 
-    display() {
-        super.display();
-        console.log("Salary:", this.salary);
-    }
-}
+// setTimeout(() => {
+//    console.log("My name is Dhruv Sharma") 
+// }, 5000);
 
-// Object creation
-let account = new SalaryAccount(
-    "Dhruv",
-    123456,
-    50000,
-    40000
-);
+// let prom=new Promise((resolve,reject)=>{
+// if(complete){
+//     resolve("I am true")
+// }else{
+//     reject("False")
+// }
+// })
 
-account.display();
 
-account.calculateSalaryAfterWithdraw(10000);
+    // const EventEmitter=require('events');
+// const ud=new EventEmitter();
+// ud.on('greet',(name)=>{
+//     console.log(`hello there ${name}`)
+// })
 
-setTimeout(() => {
-   console.log("My name is Dhruv Sharma") 
-}, 5000);
+// ud.emit('exit',(num)=>{
+//     console.log(`BYE ${num}`);
+// })
+
+// ud.on('greet','Dhruv');
+// ud.emit('emit','fhdui');
+
+// const EventEmitter=require("events");
+
+// class Button extends EventEmitter{
+//     click(){
+//         console.log("Button Clicked");
+//         this.emit('click',{});
+//     }
+// }
+// const button =new Button();
+// button.on("click",()=>{
+//     console.log("Button Cliocked")
+// })
+
+// button.click();
+
+
+console.log("hi");
+setTimeout(()=>{
+    console.log("hello")
+},5000);
+
+setImmediate(()=>{
+    console.log("Immediate");
+})
+
+process.nextTick(()=>{
+    console.log("nt");
+})
